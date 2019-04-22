28a1fc9e414961be09622dcca51d1c9370e0de15
Anton Citko <acecitko@yahoo.com>
2019-04-22 13:05:51 -0700

    Added basic game and player classes with relvant variables

--- Session 2P - Rock, Paper, Scissors Plus/Session2P_Anton.py
+++ Session 2P - Rock, Paper, Scissors Plus/Session2P_Anton.py
@@ -1,7 +1,31 @@
+import random
 
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        
+    def start_game(self):
+        # game loop
+        pass
 
+   
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.m_current = "NA"
+        
+        # How many consecutive turns have had the same move
+        # Resets to 1 evertime a new move is played
+        # Adds one if same move from previous turn is played 
+        self.prev_same_moves = 0
+        
+    def prompt_turn(self):
+        pass
+        
 
-
+g1 = Game(Player(input("What is Player 1's name? ")), Player(input("What is Player 2's name? ")))
 
 
 
